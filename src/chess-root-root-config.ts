3f8acf36c363785cced239712a3b304c409a3dca
MOnichshuk1 <monichshuk@dar.io>
2023-11-15 19:59:45 +0600

    added chess-bpard mf

--- src/chess-root-root-config.ts
+++ src/chess-root-root-config.ts
@@ -1,19 +1,22 @@
 import { registerApplication, start, LifeCycles } from "single-spa";
 
+// registerApplication({
+//   name: "@single-spa/welcome",
+//   app: () =>
+//     System.import<LifeCycles>(
+//       "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
+//     ),
+//   activeWhen: ["/"],
+// });
+
 registerApplication({
-  name: "@single-spa/welcome",
+  name: "@chess/board",
   app: () =>
     System.import<LifeCycles>(
-      "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
+      "@chess/board"
     ),
-  activeWhen: ["/"],
+  activeWhen: ["/"]
 });
-
-// registerApplication({
-//   name: "@chess-root/navbar",
-//   app: () => System.import("@chess-root/navbar"),
-//   activeWhen: ["/"]
-// });
 
 start({
   urlRerouteOnly: true,
