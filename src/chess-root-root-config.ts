2cae1a0640b715e9cc212c1835288810a0db1508
MOnichshuk1 <monichshuk@dar.io>
2023-11-15 22:10:45 +0600

    added mui in board

--- src/chess-root-root-config.ts
+++ src/chess-root-root-config.ts
@@ -1,14 +1,5 @@
 import { registerApplication, start, LifeCycles } from "single-spa";
 
-// registerApplication({
-//   name: "@single-spa/welcome",
-//   app: () =>
-//     System.import<LifeCycles>(
-//       "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
-//     ),
-//   activeWhen: ["/"],
-// });
-
 registerApplication({
   name: "@chess/board",
   app: () => System.import<LifeCycles>("@chess/board"),
